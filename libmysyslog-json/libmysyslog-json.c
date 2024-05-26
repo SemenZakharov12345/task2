c006ce4d321ee8c7479b426a81265754086d6cfe
artr1ti0n <ivan.khasanov.2016@mail.ru>
2024-05-26 17:30:53 +0300

    Updated libmysyslog-json.c

--- libmysyslog-json/libmysyslog-json.c
+++ libmysyslog-json/libmysyslog-json.c
@@ -1,5 +1,15 @@
-#include "libmysyslog.h"
+#include "libmysyslog-json.h"
+#include <stdio.h>
+#include <time.h>
 
-int json_log(const char* msg, int level, const char* path) {
-    return mysyslog(msg, level, 1, 1, path);
+static const char* log_levels[] = {"DEBUG", "INFO", "WARN", "ERROR", "CRITICAL"};
+
+int log_to_json(const char* msg, int level, const char* path) {
+    FILE* file = fopen(path, "a");
+    if (!file) return -1;
+
+    time_t now = time(NULL);
+    fprintf(file, "{\"timestamp\":%ld,\"log_level\":\"%s\",\"message\":\"%s\"}\n", now, log_levels[level], msg);
+    fclose(file);
+    return 0;
 }

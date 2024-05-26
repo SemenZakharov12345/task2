c0ae25ef0e798199f341fcbc4c06e51ed45f3b91
artr1ti0n <ivan.khasanov.2016@mail.ru>
2024-05-26 17:31:09 +0300

    Updated libmysyslog-json.h

--- libmysyslog-json/libmysyslog-json.h
+++ libmysyslog-json/libmysyslog-json.h
@@ -1,6 +1,6 @@
 #ifndef LIBMYSYSLOG_JSON_H
 #define LIBMYSYSLOG_JSON_H
 
-int json_log(const char* msg, int level, const char* path);
+int log_to_json(const char* msg, int level, const char* path);
 
 #endif // LIBMYSYSLOG_JSON_H

986e7d6340f9ce0abc5b18b8c29a49175aac9162
artr1ti0n <ivan.khasanov.2016@mail.ru>
2024-05-26 17:29:44 +0300

    Updated libmysyslog-text.h

--- libmysyslog-text/libmysyslog-text.h
+++ libmysyslog-text/libmysyslog-text.h
@@ -1,6 +1,6 @@
 #ifndef LIBMYSYSLOG_TEXT_H
 #define LIBMYSYSLOG_TEXT_H
 
-int text_log(const char* msg, int level, const char* path);
+int log_to_text(const char* msg, int level, const char* path);
 
 #endif // LIBMYSYSLOG_TEXT_H

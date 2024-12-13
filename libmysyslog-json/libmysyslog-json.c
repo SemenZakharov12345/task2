ebc81274fa54143a062262c4b02147177ff2a2ed
SemenZakharov12345 <semen.zaharov234@gmail.com>
2024-12-13 19:55:30 +0300

    Update libmysyslog-json.c

--- libmysyslog-json/libmysyslog-json.c
+++ libmysyslog-json/libmysyslog-json.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 static const char* log_levels[] = {"DEBUG", "INFO", "WARN", "ERROR", "CRITICAL"};
-
+ 
 int log_to_json(const char* msg, int level, const char* path) {
     FILE* file = fopen(path, "a");
     if (!file) return -1;
